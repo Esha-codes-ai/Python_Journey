9d1ded2b43912754770d2f3aade96f7cae592435
Esha <esha03657@gmail.com>
2026-06-20 18:44:28 +0530

    update

---
+++ data_types.py/Numbers.py
@@ -0,0 +1,7 @@
+# number is group of objects
+# Set, complex, fraction, boolean, etc are numbers
+x=5
+y=10
+z=34
+print(x+y+z)
+print(x+y*z) #not work in real world production 
--- data_types.py/list.py
+++ data_types.py/list.py
@@ -1,6 +1,7 @@
 # List store data in a continuos way.
 # Its data is in brackets[].
 # List starts it count from zero 
+# List is mutable 
 # eg
 
 list=[1,2,3,4,5]
@@ -17,3 +18,18 @@ Largest=max(List1)
 print("The Largest number of the List1 is:", Largest)
 Average=sum(List1)/len(List1)
 print("The average of the List1 is:", Average)
+
+# reference one list with other 
+L1=[1,2,3]
+L2=L1
+print(L1)
+L1[0]=55
+print(L1)
+print(L2)
+
+# both reference of object is different
+n=[1,2,3]
+m=n
+m==n
+m is n
+# 
