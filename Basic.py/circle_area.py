4d1b7bf0911597b3eb0f8f359e9cfe018d4cc7a0
Esha <esha03657@gmail.com>
2026-06-22 22:26:03 +0530

    update

---
+++ Basic.py/circle_area.py
@@ -0,0 +1,11 @@
+# predefined value of radius and unit
+radius=45
+unit="cm"
+area_of_circle=float(3.14*radius**2)
+print("The area of the circle is:", area_of_circle, unit)
+
+# take the input from the user 
+radius=float(input("Enter the radius of the circle is"))
+unit=input("Enter the measurement unit of the cicle:")
+area=float(3.14*radius**2)
+print("The area of the circle is :", area, unit)
