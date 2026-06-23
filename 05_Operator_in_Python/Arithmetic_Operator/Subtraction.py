7d836008c5eff3120dabd1f2de86585629c092a5
Esha <esha03657@gmail.com>
2026-06-23 23:03:48 +0530

    update

---
+++ 05_Operator_in_Python/Arithmetic_Operator/Subtraction.py
@@ -0,0 +1,10 @@
+# subtract operator is used to subtract two values.
+# Predefined values
+a=10
+b=40
+print(a-b)
+
+# Taking input from the users
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))   
+print("The difference of two numbers is: ", a-b)
