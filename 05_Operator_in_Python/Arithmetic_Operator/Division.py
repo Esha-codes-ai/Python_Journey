9a69837337d7f9e5dcbe0fc5b13a8b652bb934db
Esha <esha03657@gmail.com>
2026-06-23 23:11:08 +0530

    update

---
+++ 05_Operator_in_Python/Arithmetic_Operator/Division.py
@@ -0,0 +1,11 @@
+# Division is used for divide the two values.
+# it gives the float value as output.
+# predefined values
+value1=1000
+value2=5.0
+print(value1/value2)
+
+# Taking input from the users
+value1=int(input("Enter the first number: "))
+value2=int(input("Enter the second number: "))
+print("The division of two numbers is: ", value1/value2)
