ec7ad70c641042f8b0c223fee1af240310897a0d
Esha <esha03657@gmail.com>
2026-06-23 23:24:29 +0530

    update

---
+++ 05_Operator_in_Python/Arithmetic_Operator/Floor_Division_operator.py
@@ -0,0 +1,10 @@
+# floor division gives the integer value as output.
+# predefined values
+value1=1000
+value2=9
+print(value1//value2)
+
+# Taking input from the users
+value1=int(input("Enter the first number: "))
+value2=int(input("Enter the second number: "))
+print("The floor division of two numbers is: ", value1//value2)
