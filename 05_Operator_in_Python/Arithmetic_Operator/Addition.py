cb3ae3cdc732848326abcdba165a908b6267ccc7
Esha <esha03657@gmail.com>
2026-06-23 23:00:29 +0530

    update

---
+++ 05_Operator_in_Python/Arithmetic_Operator/Addition.py
@@ -0,0 +1,10 @@
+# Addition is used to add two values.
+# predefined values
+a=10
+b=40
+print(a+b)
+
+# Taking input from the users
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+print("The sum of two numbers is: ", a+b)
