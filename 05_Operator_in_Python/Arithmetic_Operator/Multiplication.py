354103acb2e293319e68c8ab46f63712c3efc6fd
Esha <esha03657@gmail.com>
2026-06-23 23:06:49 +0530

    update

---
+++ 05_Operator_in_Python/Arithmetic_Operator/Multiplication.py
@@ -0,0 +1,12 @@
+# Multiplication operator is used to multiply two values.
+# In string it is used to repeat the string for n times.
+# also for concatenation of list and tuple.
+# Predefined values
+value1=10
+value2=50
+print(value1*value2)
+
+# Taking input from the users
+value1=int(input("Enter the first number: "))
+value2=int(input("Enter the second number: "))
+print("The product of two numbers is: ", value1*value2)
