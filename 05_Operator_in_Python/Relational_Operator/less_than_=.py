034c48c928aa4fd6acf509c37299f89c75e30136
Esha <esha03657@gmail.com>
2026-06-24 21:44:18 +0530

    update

---
+++ 05_Operator_in_Python/Relational_Operator/less_than_=.py
@@ -0,0 +1,4 @@
+# less than (<=) is used to compare two values. It returns True if the left value is less than or equal to the right value, otherwise it returns False.
+value1=100
+value2=400
+print(value1<=value2)  # Output: True
