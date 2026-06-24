779ea0b66108d979c7679572a809d1c324226ded
Esha <esha03657@gmail.com>
2026-06-24 20:31:31 +0530

    update

---
+++ 05_Operator_in_Python/Relational_Operator/Equal_operator.py
@@ -0,0 +1,9 @@
+# Equal to operator in python used to compare two values.
+value1=100
+value2=100
+print(value1==value2)
+
+# Taking input from the users
+value1=int(input("enter value1:"))
+value2=int(input("enter value2:"))
+print(value1==value2)
