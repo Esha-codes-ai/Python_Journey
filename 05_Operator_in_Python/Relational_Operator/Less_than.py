3f6a30e5dcc385822a84717cf03b38acbe22b0a7
Esha <esha03657@gmail.com>
2026-06-24 21:15:14 +0530

    update

---
+++ 05_Operator_in_Python/Relational_Operator/Less_than.py
@@ -0,0 +1,9 @@
+# less than(<) operator is used to compare two values.
+value1=100
+value2=400
+print(value1<value2)
+
+# taking input from user
+value1=int(input("Enter first value: "))
+value2=int(input("Enter second value: "))
+print(value1<value2)  # Output: True or False based on user input
