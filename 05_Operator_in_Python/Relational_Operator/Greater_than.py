8a05d88f995a8714b9691c64f47a2dff99a324ae
Esha <esha03657@gmail.com>
2026-06-24 20:58:22 +0530

    update

---
+++ 05_Operator_in_Python/Relational_Operator/Greater_than.py
@@ -0,0 +1,8 @@
+# Greater than (>) operator is used to compare two values. It returns True if the left value is greater than the right value, otherwise it returns False.
+value1=10
+value2=5
+print(value1>value2)  # Output: True
+# Taking input from user
+value1=int(input("Enter first value: "))
+value2=int(input("Enter second value: "))
+print(value1>value2)  # Output: True or False based on user input
