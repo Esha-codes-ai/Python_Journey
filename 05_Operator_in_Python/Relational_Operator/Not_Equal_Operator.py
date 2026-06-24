f499499dc22a916c89a548dd14670dd6062b2760
Esha <esha03657@gmail.com>
2026-06-24 20:34:42 +0530

    update

---
+++ 05_Operator_in_Python/Relational_Operator/Not_Equal_Operator.py
@@ -0,0 +1,9 @@
+# Not Equal Operator check values are not eqaul.
+# It return true or false
+value1=1000
+value2=500
+print(value1!=value2)
+# Taking input from the users
+value1=int(input("enter value1:"))
+value2=int(input("enter value2:"))
+print(value1!=value2)
