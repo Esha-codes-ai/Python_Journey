9c5e16329b452af450ad9dc403343a726c06f969
Esha <esha03657@gmail.com>
2026-06-25 23:03:01 +0530

    update

---
+++ 05_Operator_in_Python/Logical_Operator/NOT.py
@@ -0,0 +1,9 @@
+# Logical NOT operator is used to reverse the boolean value.
+# It returns TRUE if the condition is FALSE and vice versa.
+
+# Example
+x = True
+y = False
+
+print(not x)  
+print(not y)  
