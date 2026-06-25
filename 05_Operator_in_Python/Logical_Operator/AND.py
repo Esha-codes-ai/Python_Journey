3bb113334ad628af0760233fa84a245dce6bddbf
Esha <esha03657@gmail.com>
2026-06-25 22:54:56 +0530

    update

--- 05_Operator_in_Python/Logical_Operator/AND.py
+++ 05_Operator_in_Python/Logical_Operator/AND.py
@@ -9,3 +9,11 @@ if age>=18 and age<=100:
     print("You are eligible to vote")
 else:
     print("You are not eligible to vote")
+
+# Student pass or fail
+Student=str(input("Enter your name:"))
+Marks= int(input("Enter your marks:"))
+if Marks>40 and Marks<=100:
+    print("Congratulations",Student,"You are pass")
+else:
+    print("Keep HardWorking", Student,"Do well at next time")    
---
+++ 05_Operator_in_Python/Logical_Operator/OR.py
@@ -0,0 +1,11 @@
+# Logical OR operator is used to 
+# return TRUE if only and atleast one condition is true.
+# if both the conditions are false then it return false.
+
+maths = 40
+science = 25
+
+if maths >= 33 or science >= 33:
+    print("Pass")
+else:
+    print("Fail")
