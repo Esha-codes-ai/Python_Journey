2673d3bd26f4450665d41d1edfb88467ab837593
Esha <esha03657@gmail.com>
2026-06-27 22:56:12 +0530

    update

---
+++ 05_Operator_in_Python/Bitwise_Operator/XOR_in_bitwise.py
@@ -0,0 +1,5 @@
+# bitwise (^) is a very important operator in python which is used to perform XOR operation on two integers. It returns 1 if the bits are different and 0 if they are the same.
+a = 5  # binary: 101    
+b = 3  # binary: 011
+result = a ^ b  # binary: 110, decimal: 6
+print("The result of XOR operation between", a, "and", b, "is:", result)
