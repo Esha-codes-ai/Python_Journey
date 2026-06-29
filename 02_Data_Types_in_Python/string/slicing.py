d07a5ebf36a9546746486206527b1e133a59a7aa
Esha <esha03657@gmail.com>
2026-06-29 23:14:23 +0530

    update

---
+++ 02_Data_Types_in_Python/string/slicing.py
@@ -0,0 +1,7 @@
+# # Slicing is used to extract the specific part of the string without change the original string.
+# # string[start : end : step]
+# start → Kahan se start karna hai.
+# end → Kahan tak lena hai (end index include nahi hota).
+# step → Kitne characters skip karne hain.
+name="Pythonisthebestprogrammminglanguage"
+print(name[0:10:2])
