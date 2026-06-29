362e602678f3ff7b42d2ff5cc86e7c3789d1b109
Esha <esha03657@gmail.com>
2026-06-29 22:33:22 +0530

    update

---
+++ 02_Data_Types_in_Python/string/index.py
@@ -0,0 +1,9 @@
+# Indexing helps to know the correct index of the string.
+
+Code="Python"
+print(Code[0])
+print(Code[1])
+print(Code[2])
+print(Code[3])
+print(Code[4])
+print(Code[5])
---
+++ 02_Data_Types_in_Python/string/initialize_string.py
@@ -0,0 +1,7 @@
+# String is a sequence of characters in double, single or triple 
+# quotation marks.
+# -----------we initialize the string using "" ,'', """"""------------
+Name="Esha"
+country="India"
+State="Uttarakhand"
+print(Name,country,State)
