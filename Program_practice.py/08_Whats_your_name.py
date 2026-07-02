4b5db64f00b7fb7dc42c3004a963e2413544b0c7
Esha <esha03657@gmail.com>
2026-07-02 22:43:25 +0530

    update

---
+++ Program_practice.py/08_Whats_your_name.py
@@ -0,0 +1,6 @@
+def print_name(first_name, last_name):
+    print(f"Hello, {first_name} from {last_name}!", "Welcome to the python programming!")
+if __name__ == "__main__":
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+    print_name(first_name, last_name)
