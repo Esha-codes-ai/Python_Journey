338bb7d3867f273544e1d3669c9e8f76635df6fe
Esha <esha03657@gmail.com>
2026-07-03 20:02:05 +0530

    update

---
+++ 03_Operator_in_Python/Ternary_Operator.py
@@ -0,0 +1,6 @@
+# ternary operator check the condition and return the value 
+# based on the condittion
+# uses if and else statement in a single line
+a=int(input())
+result="Even" if a%2==0 else "Odd"
+print(result)
