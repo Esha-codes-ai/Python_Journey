2227675fdcfbfb4017ade747cd24dd790334111c
Esha <esha03657@gmail.com>
2026-07-06 22:12:02 +0530

    update

---
+++ 02_Data_Types_in_Python/dictionary.py/4_get.py
@@ -0,0 +1,9 @@
+students={
+    "Name":"John",
+    "Age":20,
+    "Course":"Python",
+    "Marks":90,
+    "Father's Name":"David"
+}
+print(students) # to access the whole dictionary.
+print(students.get("Name"))
