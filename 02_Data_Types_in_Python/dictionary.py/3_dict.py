fa8fed918ad442e82251780be6663599fed81cef
Esha <esha03657@gmail.com>
2026-07-06 22:09:09 +0530

    update

---
+++ 02_Data_Types_in_Python/dictionary.py/3_dict.py
@@ -0,0 +1,17 @@
+print("Dictionary is a mutable data structure that stores the key value pairs.each key is unique and is used to access the corresponding value.")
+# Dictionary is create just like a english dictonary having key and values
+# It is very important data structure in python and is used in many applications.
+Dictionary={
+    "Key":"Value"
+}
+print(Dictionary) # to access the whole dictionary.
+
+# students details 
+students={
+    "Name":"John",
+    "Age":20,
+    "Course":"Python",
+    "Marks":90,
+    "Father's Name":"David"
+}
+print(students) # to access the whole dictionary.
