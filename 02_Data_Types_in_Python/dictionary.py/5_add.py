73e9bc219a5ca73193db2b77d5d093be51c48a53
Esha <esha03657@gmail.com>
2026-07-08 22:28:44 +0530

    update

---
+++ 02_Data_Types_in_Python/dictionary.py/5_add.py
@@ -0,0 +1,8 @@
+# add items in dictionary
+students={
+    "Name":"Esha",
+}
+students["Name"] = "John" # to update the value of an existing key.
+students["Age"] = 20 # to add a new key-value pair.
+students["Course"] = "Python"
+print(students) # to access the whole dictionary.
---
+++ 02_Data_Types_in_Python/dictionary.py/6_del.py
@@ -0,0 +1,11 @@
+Photos={
+    "city":"Noida, Greater Noida, Pune, Bangalore",
+    "Nature":"Mountains, Rivers, Lakes, Waterfalls",
+    "Animals":"Dogs, Cats, Birds, Fish",
+}
+del Photos["Animals"]
+print(Photos)
+del Photos["city"]
+print(Photos)
+del Photos["Nature"]
+print(Photos)
