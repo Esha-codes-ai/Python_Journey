6ad0c20c2b1288da7dfedd466b32da7c953fd9f6
Esha <esha03657@gmail.com>
2026-07-09 11:43:55 +0530

    update

---
+++ 02_Data_Types_in_Python/dictionary.py/10_pop.py
@@ -0,0 +1,7 @@
+# pop is used to remove the specified item from the dictionary.
+dictionary={
+    "AI":"Artificial Intelligence",
+    "ML":"Machine Learning",
+    "DL":"Deep Learning"
+}
+print(dictionary.pop("AI")) # to remove the specified item from the dictionary.
---
+++ 02_Data_Types_in_Python/dictionary.py/9_values.py
@@ -0,0 +1,7 @@
+# Values is used to access the values of the dictionary.
+dictionary={
+    "AI":"Artificial Intelligence",
+    "ML":"Machine Learning",
+    "DL":"Deep Learning"
+}
+print(dictionary.values())
