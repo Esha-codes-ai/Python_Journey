5f92a08cdd639144620674af5785bc7de808e7e3
Esha <esha03657@gmail.com>
2026-07-18 21:48:52 +0530

    update

---
+++ Program_practice.py/19_Weather_Activity.py
@@ -0,0 +1,10 @@
+# lets check your weather 
+User_input=input("Please enter the weather condition you want to check:")
+if User_input=="Sunny":
+    print("You can go for a walk.")
+elif User_input=="Rainy":
+    print("You should take an umbrella.")
+elif User_input=="Cloudy":
+    print("It might rain later.")
+else:
+    print("please re-check the weather condition.")
