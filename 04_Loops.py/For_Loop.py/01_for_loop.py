363b36f1fc6efa2359a4c8ce019694a75e344b69
Esha <esha03657@gmail.com>
2026-07-22 22:05:22 +0530

    update

---
+++ 04_Loops.py/For_Loop.py/01_for_loop.py
@@ -0,0 +1,9 @@
+# FOR LOOP IS USED TO ITERATE THE SEQUENCE OF NUMBERS, LISTS, TUPLES, DICTIONARIES, SETS, AND STRINGS.
+# IT IS ITERATE OVER THE SEQUENCE OF ELEMENTS .
+# SYNTAX= for i in sequnce:
+People= ["Esha", "Bobby", "Satish","Sunita"]
+for i in People:
+    print(i) # print all names in people list
+    
+    print(len(i)) # print length of each name in people list
+     
