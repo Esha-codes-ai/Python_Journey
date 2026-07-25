b70147be8408101d5f73536159a70e2b50f14889
Esha <esha03657@gmail.com>
2026-07-25 22:28:40 +0530

    update

--- 04_Loops.py/For_Loop.py/Print 10 Number.py
+++
@@ -1,4 +0,0 @@
-Number=int(input("Enter the number:"))
-# for i in range(1,Number): # print till Number-1
-for i in range(1,Number+1): # print till Number
-    print(i)
---
+++ 04_Loops.py/while_loop.py
@@ -0,0 +1,8 @@
+# while condition:
+    # Code block to execute while the condition is True
+    # Update the condition or perform actions to eventually break the loop
+count=1
+while count<=5:
+    print("Esha Joshi")
+    count+=1
+print(count)
